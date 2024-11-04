1886f17020d86da78e408b785a04f1870573f4e6
financeRyu <fb0982@naver.com>
2024-11-04 11:59:51 +0900

    test : git commit test

---
+++ String/문자_찾기.java
@@ -0,0 +1,12 @@
+public class 문자_찾기 {
+
+    public static void main(String[] args) {
+        문자_찾기 obj = new 문자_찾기();
+        obj.solution();
+    }
+
+    public void solution(){
+        System.out.println("Hello World");
+    }
+
+}
